ce95e3e63a0c053236800bde7c06e206eb42613c
Amiel Cohen <amielc1@gmail.com>
2024-03-06 16:38:12 +0200

    Request-Reply in Messaging

--- Nats.Sender/Program.cs
+++ Nats.Sender/Program.cs
@@ -1,34 +1,31 @@
-﻿using System;
-using NATS.Client;
+﻿using NATS.Client;
 
-class PublisherProgram
+
+//new SendRequestReply().Start();
+
+var url = "nats://localhost:4222";
+var subject = "test.subject";
+
+// Create a connection factory
+var connectionFactory = new ConnectionFactory();
+
+// Create a connection to the NATS server
+using (var connection = connectionFactory.CreateConnection(url))
 {
-    static void Main(string[] args)
+    // Function to publish a message
+    void PublishMessage(string message)
     {
-        var url = "nats://localhost:4222";
-        var subject = "test.subject";
-
-        // Create a connection factory
-        var connectionFactory = new ConnectionFactory();
-
-        // Create a connection to the NATS server
-        using (var connection = connectionFactory.CreateConnection(url))
-        {
-            // Function to publish a message
-            void PublishMessage(string message)
-            {
-                connection.Publish(subject, System.Text.Encoding.UTF8.GetBytes(message));
-                Console.WriteLine($"Message published to '{subject}': {message}");
-            }
-
-            // Example messages
-            PublishMessage("Hello, NATS! This is message 1.");
-            PublishMessage("Hello, NATS! This is message 2.");
-            PublishMessage("Hello, NATS! This is message 3.");
-
-            // Keep the console window open
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
-        }
+        connection.Publish(subject, System.Text.Encoding.UTF8.GetBytes(message));
+        Console.WriteLine($"Message published to '{subject}': {message}");
     }
+
+    // Example messages
+    PublishMessage("Hello, NATS! This is message 1.");
+    PublishMessage("Hello, NATS! This is message 2.");
+    PublishMessage("Hello, NATS! This is message 3.");
+
+    // Keep the console window open
+    Console.WriteLine("Press any key to exit...");
+    Console.ReadKey();
 }
+ 
--- Nats.Subscriber/Program.cs
+++ Nats.Subscriber/Program.cs
@@ -1,34 +1,29 @@
 ﻿using NATS.Client;
 
-class Program
+
+var url = "nats://localhost:4222";
+var topic = "test.subject";
+// Create a connection factory
+var connectionFactory = new ConnectionFactory();
+
+// Create a synchronous connection to the NATS server
+using (var connection = connectionFactory.CreateConnection(url))
 {
-    static void Main(string[] args)
+    // Subscribe to a subject
+    Console.WriteLine($"Subscribe to Topic {topic}");
+    var subscription = connection.SubscribeAsync(topic);
+    subscription.MessageHandler += (sender, e) =>
     {
-        var url = "nats://localhost:4222";
-        var topic = "test.subject";
-        // Create a connection factory
-        var connectionFactory = new ConnectionFactory();
+        Console.WriteLine($"Received message:  {e.Message.Subject}  || {System.Text.Encoding.UTF8.GetString(e.Message.Data)}");
+    };
+    subscription.Start();
+    Console.WriteLine("Waiting for messages");
+    // Publish a message to the subject
+    //string message = "Hello, NATS!";
+    //connection.Publish("test.subject", System.Text.Encoding.UTF8.GetBytes(message));
+    //Console.WriteLine("Message published.");
 
-        // Create a synchronous connection to the NATS server
-        using (var connection = connectionFactory.CreateConnection(url))
-        {
-            // Subscribe to a subject
-            Console.WriteLine($"Subscribe to Topic {topic}");
-            var subscription = connection.SubscribeAsync(topic);
-            subscription.MessageHandler += (sender, e) =>
-            {
-                Console.WriteLine($"Received message:  {e.Message.Subject}  || {System.Text.Encoding.UTF8.GetString(e.Message.Data)}");
-            };
-            subscription.Start();
-            Console.WriteLine("Waiting for messages");
-            // Publish a message to the subject
-            //string message = "Hello, NATS!";
-            //connection.Publish("test.subject", System.Text.Encoding.UTF8.GetBytes(message));
-            //Console.WriteLine("Message published.");
-
-            // Keep the console window open
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
-        }
-    }
+    // Keep the console window open
+    Console.WriteLine("Press any key to exit...");
+    Console.ReadKey();
 }
